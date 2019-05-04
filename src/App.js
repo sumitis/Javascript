d931646626f35e9b2544fab6714060c477f017e0
sumit kumar <sumit.kumar@groupbima.com>
2019-05-04 18:58:51 +0530

    5_min_evening_work

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends Component{
+    state={
+        title:[
+            {value:'MR',display:'MR'},
+            {value:'MRS',display:'MRS'},
+            {value:'MISS',display:'MISS'},
+    ],
+    gender:''
+    }
+    handleChange=(event)=>{
+        this.setState({ value: event.target.value });
+        if(event.target.value=='MR'){
+            this.setState({gender:'male'})
+        }else{
+            this.setState({gender:'female'})
+        }
+    }
+    
+    render(){
+        const {title,value,gender}=this.state
+        return(
+            <div>
+                <h1>Hello {value}</h1>
+                <select onChange={this.handleChange} value={value} >
+                    {this.state.title.map(item=> 
+                    
+                    <option key={item.value} value={item.value}>{item.display}</option>
+                   
+                    )}
+                    </select>
+                    Gender:
+        <div>
+          <input type="radio" checked={gender == "male"} 
+          value="male" /> Male
+          <input type="radio" checked={gender == "female"} 
+              value="female"  /> Female
+        </div>
+               { "Select Gender: " } {gender}
+      
+            </div>
+        )
+    }
 }
-
-export default App;
+export default App
